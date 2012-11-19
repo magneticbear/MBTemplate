1b2a6fd8434a706309b4030e6dac68fce43a3c2b
JP Simard <jp@jpsim.com>
2012-11-19 16:13:18 -0500

    Initial Commit

---
+++ MBTemplate/MBAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MBAppDelegate.h
+//  MBTemplate
+//
+//  Created by Jean-Pierre Simard on 11/19/12.
+//  Copyright (c) 2012 Magnetic Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MBViewController;
+
+@interface MBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MBViewController *viewController;
+
+@end
---
+++ MBTemplate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MBTemplate
+//
+//  Created by Jean-Pierre Simard on 11/19/12.
+//  Copyright (c) 2012 Magnetic Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
